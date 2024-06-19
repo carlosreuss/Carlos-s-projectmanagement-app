6451c08273024c06b81af37be0334c54534ff16b
carlosreuss <20creuss@wakatipu.school.nz>
2024-06-19 14:56:27 +1200

    working templates with css

--- DJAN/theminimanagement/urls.py
+++ DJAN/theminimanagement/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('jobs/', views.jobs, name='jobs'),
+    path('map/', views.map, name='map'),
+    path('newjob', views.new_job, name='new_job'),
+    path('job/edit/', views.edit, name='edit')
     # other paths...
 ]
--- DJAN/theminimanagement/views.py
+++ DJAN/theminimanagement/views.py
@@ -10,5 +10,21 @@ import random
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'theminimanagement/index.html')
+
+def jobs(request):
+    return render(request, 'theminimanagement/jobs.html')
+
+def map(request):
+    return render(request, 'theminimanagement/map.html')
+
+def new_job(request):
+    return render(request, 'theminimanagement/new_job.html')
+
+def edit(request):
+    return render(request, 'theminimanagement/edit.html')
+
+
+
+
 
