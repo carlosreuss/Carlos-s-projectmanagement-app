5438fc710128af574eca4f1ed61aefeda9f98905
carlosreuss <20creuss@wakatipu.school.nz>
2024-08-25 21:48:20 +1200

    made the data for the graphs a json file

--- DJAN/theminimanagement/views.py
+++ DJAN/theminimanagement/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render , redirect
-
+import json
+from django.shortcuts import render
+import os
+from django.conf import settings
 # Create your views here.
 
 def index(request):
-    return render(request, 'theminimanagement/index.html')
+    file_path = os.path.join(settings.BASE_DIR, 'theminimanagement', 'static', 'js', 'main_chart.json')
+    with open(file_path, 'r') as file:
+        chart_data = json.load(file)
+
+
+    total_sum = sum(chart_data['datasets'][0]['data'])
+
+    combined_data = []
+    for i in range(len(chart_data['labels'])):
+        value = chart_data['datasets'][0]['data'][i]
+        percentage = (value / total_sum) * 100
+
+        combined_data.append((chart_data['labels'][i], percentage))
+    
+    return render(request, 'theminimanagement/index.html', {
+        'combined_data': combined_data,
+        'chart_data': json.dumps(chart_data)
+    })
 
 def jobs(request):
     return render(request, 'theminimanagement/jobs.html')
@@ -16,8 +35,3 @@ def new_job(request):
 
 def edit(request):
     return render(request, 'theminimanagement/edit.html')
-
-
-
-
-
