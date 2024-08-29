13b5823d6a10445178e6f46abbb3ba823f7cdaf1
carlosreuss <20creuss@wakatipu.school.nz>
2024-08-29 20:23:34 +1200

    I competed the jobs page and new job

--- DJAN/theminimanagement/views.py
+++ DJAN/theminimanagement/views.py
@@ -1,5 +1,5 @@
 import json
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import os
 from django.conf import settings
 # Create your views here.
@@ -9,7 +9,6 @@ def index(request):
     with open(file_path, 'r') as file:
         chart_data = json.load(file)
 
-
     total_sum = sum(chart_data['datasets'][0]['data'])
 
     combined_data = []
@@ -18,8 +17,24 @@ def index(request):
         percentage = (value / total_sum) * 100
 
         combined_data.append((chart_data['labels'][i], percentage))
+
+    file_path_two = os.path.join(settings.BASE_DIR, 'theminimanagement', 'static', 'js', 'job_data.json')
+    with open(file_path_two, 'r') as file:
+        chart_data_two = json.load(file)
+
+    job_names = []
+    job_pro = []
+
+    for i in range(len(chart_data_two['jobs'])):
+        job_name = chart_data_two['jobs'][i]['labels']
+        job_progress = chart_data_two['jobs'][i]['data']
+        job_names.append(job_name)
+        job_pro.append(job_progress)
+        print(job_names, job_pro)
     
     return render(request, 'theminimanagement/index.html', {
+        'job_names': job_names,
+        'job_pro': job_pro,
         'combined_data': combined_data,
         'chart_data': json.dumps(chart_data)
     })
@@ -31,13 +46,35 @@ def jobs(request):
 
     jobs = data.get('jobs', [])
 
-
     return render(request, 'theminimanagement/jobs.html', {'jobs': jobs})
 
 def map(request):
     return render(request, 'theminimanagement/map.html')
 
 def new_job(request):
+    if request.method == 'POST':
+        job_name = request.POST.get('name')
+        job_street = request.POST.get('address')
+        job_town = request.POST.get('town')
+        job_post_code = request.POST.get('post-code')
+        job_progresstion = "0"
+
+        job_address = job_street + "," + " " + job_town + " " + job_post_code
+
+        
+
+        file_path_three = os.path.join(settings.BASE_DIR, 'theminimanagement', 'static', 'js', 'job_data.json')
+        with open(file_path_three, 'r+') as file:
+            data_add_job = json.load(file)
+
+            new_job = {"labels": job_name, "address": job_address, "data": "0"}
+
+            data_add_job['jobs'].append(new_job)
+            file.seek(0)
+            json.dump(data_add_job, file, indent=4)
+        return redirect('jobs')
+
+
     return render(request, 'theminimanagement/new_job.html')
 
 def edit(request):
